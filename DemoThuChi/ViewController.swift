26eaf1dbb403b6796d4ca0de9e3d2bc260446c1c
congthai08 <congthai08@gmail.com>
2016-12-13 14:49:13 +0700

    Initial Commit

---
+++ DemoThuChi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoThuChi
+//
+//  Created by Huỳnh Công Thái on 12/13/16.
+//  Copyright © 2016 Huỳnh Công Thái. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
